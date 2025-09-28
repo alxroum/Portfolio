b32e43f669f12bd273599e472055bd40f5290efc
Alex Ro <alxroum@gmail.com>
2025-09-27 22:40:45 -0400

    lots of changes, but now have a good syle template and baseline to build from.

---
+++ canvas.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('mainCanvas');
+var ctx = canvas.getContext("2d");
+
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+ctx.
+
+canvas.style.backgroundColor = "lightgray";
